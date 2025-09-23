4588fffa4c3422d96b811ee11679f19825dad47b
Richard Brito <richardbrito116@gmail.com>
2025-09-23 15:19:19 -0400

    Initial Commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Richard Brito on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeRealTests/BeRealTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealTests.swift
+//  BeRealTests
+//
+//  Created by Richard Brito on 9/23/25.
+//
+
+import Testing
+@testable import BeReal
+
+struct BeRealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
